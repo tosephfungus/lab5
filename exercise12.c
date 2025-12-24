056d1a76074ae908d4c177ea63f8f53ba915c9d9
toe <122792309+tosephfungus@users.noreply.github.com>
2025-12-24 03:00:45 +0000

    GCD and LCM of two integers

--- exercise12.c
+++ exercise12.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+static int gcd(int a, int b) {
+	while (b != 0) {
+		int t = b;
+		b = a % b;
+		a = t;
+	}
+	return a < 0 ? -a : a; 
+}
 
 int main(void) {
-	int n;
+	int a, b;
 
-	printf("Enter an integer: ");
-	if (scanf("%d", &n) != 1) {
+	printf("Enter two integers: ");
+	if (scanf("%d %d", &a, &b) != 2) {
 		printf("Invalid input.\n");
 		return 1;
 	}
 
-	if (n == 0) {
-		printf("All integers divide 0, so it has infinitely many factors.\n");
+	if (a == 0 && b == 0) {
+		printf("GCD and LCM are undefined when both numbers are zero.\n");
 		return 0;
 	}
 
-	int num = abs(n);
-	printf("Factors of %d:\n", n);
-	for (int i = 1; i <= num; i++) {
-		if (num % i == 0) {
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
+	int g = gcd(a, b);
+	long long lcm = (long long)a / g * b; 
+	if (lcm < 0) lcm = -lcm;
+
+	printf("GCD: %d\n", g);
+	printf("LCM: %lld\n", lcm);
 	return 0;
 }
